8f4bb5aabfcaedf223a99bbf9a0f032c987b5086
nplazas4 <45074627+nplazas4@users.noreply.github.com>
2020-05-15 04:35:41 -0500

    connection db

--- config/app.php
+++ config/app.php
@@ -256,7 +256,7 @@ return [
              */
             //'port' => 'non_standard_port_number',
             'username' => 'root',
-            'password' => '',
+            'password' => 'V3r4n02o2O',
             'database' => 'portalgeb',
             /*
              * You do not need to set this flag to use full utf-8 encoding (internal default since CakePHP 3.6).
